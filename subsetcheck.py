ce788c9b285985aaedb7ed5096b3e81f1e482a38
Pravish Bajpai <55047219+pravishbajpai06@users.noreply.github.com>
2020-08-28 23:54:47 +0530

    Update subsetcheck.py

--- subsetcheck.py
+++ subsetcheck.py
@@ -1,1 +1,18 @@
-
+def check():
+  a=int(input())
+  s=set(map(int,input().split()))
+  b=int(input())
+  c=set(map(int,input().split()))
+  l=list()
+  for i in s:
+    if i in c:
+      l.append(i)
+  l=set(l)
+  if l==s:
+    print("True")
+  else:
+    print("False")
+d=int(input())
+for  in range(d):
+  check()
+  
